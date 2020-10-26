cb0f45df0dd996b76819023fac6a4fa583ef61f8
Michael Castanieto <mcastanieto@gmail.com>
2020-10-26 00:52:35 -0700

    deploying to heroku

--- W14D1/mern-twitter/app.js
+++ W14D1/mern-twitter/app.js
@@ -6,11 +6,18 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
